d32e025227f51fa765a84a7e0685d9595b98f9a4
Mariano Longo <mariano.longo@ing.austral.edu.ar>
2020-06-03 19:30:07 -0300

    negative amount tested

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -39,6 +39,20 @@ context("Simple test", () => {
         cy.get("#beginTransactionButton").should("exist");
     });
 
+    it("Negative money is not transfered", () => {
+        cy.visit('http://localhost:3000/');
+        cy.get("#formBasicEmail").type("admin");
+        cy.get("#formBasicPassword").type("admin");
+        cy.get(".btn").click();
+
+        cy.get("#beginTransactionButton").click();
+        cy.get("#test_selector").click();
+        cy.get("#moneyAmountInput").type("-1");
+        cy.get("#transferMoneyButton").click();
+
+        cy.get("#beginTransactionButton").should("not.exist");
+    });
+
     it("Test money amount input cannot type anything except positive numbers", () => {
         cy.visit('http://localhost:3000/');
         cy.get("#formBasicEmail").type("admin");
